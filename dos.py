0f3c9a28ad880f37b212a6b2f897bdda1b86e37b
omer-X-kiyak <89635390+omer-X-kiyak@users.noreply.github.com>
2022-05-20 18:46:56 +0300

    Update dos.py

--- dos.py
+++ dos.py
@@ -2,7 +2,7 @@
 #!/usr/bin/env python3
 # Disclaimer: This script is for educational purposes only.  Do not use against any network that you don't own or have authorization to test.
 
-# We will be using the subprocess module to run commands on Kali Linux.
+# We will be using the subprocess module to run commands on Linux.
 import subprocess
 # We will require regular expressions.
 import re
